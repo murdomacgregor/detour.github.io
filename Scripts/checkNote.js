eda028e8cbbfd11b57251aab423bcbc17340393a
murdomacgregor <murdomacgregor5@gmail.com>
2021-04-19 08:36:24 +0100

    More updates

--- Scripts/checkNote.js
+++ Scripts/checkNote.js
@@ -1,7 +1,12 @@
-document.getElementById("result").innerHTML = localStorage.getItem("possessNote");
 
-if result == "yes" {
-	document.getElementById("p5").innerHTML = "You check the note you found in the outhouse. It has the numbers "501" written in what seems like blood. Maybe this could be the code for the keypad?";
-else {
-	document.getElementById("p5").innerHTML = "You don't have the first note with you. Was there a number on the note? Maybe that number is the number needed for this keypad... You will have to guess.";
-}
+var haveNote = localStorage.getItem('haveNote');
+
+function checkNote(){
+		if (haveNote == "yes") {
+			document.getElementById("p5").innerHTML = "You check the note you found in the outhouse. It has the numbers 501 written in what seems like blood. Maybe this could be the code for the keypad?";
+		}
+		
+		else {
+			document.getElementById("p5").innerHTML = "You don't have the first note with you. Was there a number on the note? Maybe that number is the number needed for this keypad... You will have to guess.";
+		}
+	}
