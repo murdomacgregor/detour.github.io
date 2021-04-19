c38cc7e91c8d5f448cb69d924a4f7d2857b124df
murdomacgregor <murdomacgregor5@gmail.com>
2021-04-19 06:58:32 +0100

    UPDATE

--- Scripts/checkNote.js
+++ Scripts/checkNote.js
@@ -1,22 +1,7 @@
-var hasNote = document.cookie;
+document.getElementById("result").innerHTML = localStorage.getItem("possessNote");
 
-function checkNote() {
-	if (hasNote == 1) {
-		document.getElementById("p5").innerHTML = "You check the note you found in the outhouse. It has the numbers "501" written in what seems like blood. Maybe this could be the code for the keypad?";
-			}
-	else {
-		document.getElementById("p5").innerHTML = "You don't have the first note with you. Was there a number on the note? Maybe that number is the number needed for this keypad... You will have to guess.";
-	}
-	
-function doSomething() {
-    var hasNote = getCookie("hasNote");
-
-    if (hasNote == null) {
-        // do cookie doesn't exist stuff;
-		document.getElementById("p5").innerHTML = "You don't have the first note with you. Was there a number on the note? Maybe that number is the number needed for this keypad... You will have to guess.";
-    }
-    else {
-        // do cookie exists stuff
-		document.getElementById("p5").innerHTML = "You check the note you found in the outhouse. It has the numbers "501" written in what seems like blood. Maybe this could be the code for the keypad?";
-    }
+if result == "yes" {
+	document.getElementById("p5").innerHTML = "You check the note you found in the outhouse. It has the numbers "501" written in what seems like blood. Maybe this could be the code for the keypad?";
+else {
+	document.getElementById("p5").innerHTML = "You don't have the first note with you. Was there a number on the note? Maybe that number is the number needed for this keypad... You will have to guess.";
 }
