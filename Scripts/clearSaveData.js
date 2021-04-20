5727170d81e7162b52421012911833562fc4c866
murdomacgregor <murdomacgregor5@gmail.com>
2021-04-20 13:50:30 +0100

    Save Game functionality

---
+++ Scripts/clearSaveData.js
@@ -0,0 +1,6 @@
+function clearSaveGame(){
+var url = localStorage.getItem("url",url);
+localStorage.removeItem("url");
+
+window.location.href="Detour BYP.html";
+}
--- Scripts/saveGame.js
+++ Scripts/saveGame.js
@@ -1,11 +1,18 @@
 
 
-function savedGame() {
+function saveGame() {
 var url=location.href;
 localStorage.setItem("url",url);
+alert('Game saved. You can click "Continue" on the main menu to resume your playthrough from here.');
 }
 
 function loadGame() {
-	localStorage.getItem("url",url);
-	window.location.href = url
+var url = localStorage.getItem("url",url);
+	if (url !== null) {
+		window.location.href = url;
+	}
+	
+	else {
+		alert('No save game data is available. Please begin a new journey by clicking "New Game"');
+	}
 }
