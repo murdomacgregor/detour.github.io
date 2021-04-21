d49e65770f0c07b0ba2e760d53bc328b179fa491
murdomacgregor <murdomacgregor5@gmail.com>
2021-04-21 13:50:13 +0100

    Detour

--- Scripts/clearSaveData.js
+++ Scripts/clearSaveData.js
@@ -1,6 +1,9 @@
 function clearSaveGame(){
+	var result = confirm("Starting a new game will erase any previously saved progress. Are you sure you would like to start a new game?");
+	if (result) {
 var url = localStorage.getItem("url",url);
 localStorage.removeItem("url");
 
 window.location.href="Detour BYP.html";
+}
 }
